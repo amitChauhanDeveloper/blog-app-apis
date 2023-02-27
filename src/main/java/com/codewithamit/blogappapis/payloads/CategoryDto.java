405130fa5a1951f8a7e31449c841081718297990
shreya.shah <shreya.s@zyapaar.com>
2023-02-27 18:47:24 +0530

    add validation cat

--- src/main/java/com/codewithamit/blogappapis/payloads/CategoryDto.java
+++ src/main/java/com/codewithamit/blogappapis/payloads/CategoryDto.java
@@ -1,5 +1,7 @@
 package com.codewithamit.blogappapis.payloads;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +10,12 @@ import lombok.NoArgsConstructor;
 public class CategoryDto {
     
     private int categoryId;
+
+    @NotEmpty
+    @Size(min = 4 ,message = "Category must be min of 4 characters !")
     private String categoryTitle;
+
+    @NotEmpty
+    @Size(min = 10, max = 500, message = "Description must be min of 10 chars and max of 500 chars !")
     private String categoryDescription;
 }
